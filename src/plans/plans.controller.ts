5cbd9b760f15675b8e9d227e21d86fee8c90ae79
Rodrigo Nogueira <rodrigoroboticsadm@gmail.com>
2025-10-29 12:01:23 -0300

    feat(plans): add route to change or remove user plan
    Adds PATCH /plans/change/:planId route, allowing users to update their plan or disconnect it. Returns the updated user with plan details.

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Patch, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Req, UseGuards } from '@nestjs/common';
 import { PlansService } from './plans.service';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
@@ -18,8 +18,20 @@ export class PlansController {
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth()
   @ApiResponse({ status: 200, description: 'Plano assinado com sucesso' })
-  async subscribe(@Param('planId') planId: string, @Request() req) {
+  @ApiResponse({ status: 400, description: 'Dados inválidos' })
+  async subscribe(@Param('planId') planId: string, @Req() req) {
     const userId = req.user.sub;
     return this.plansService.subscribe(userId, parseInt(planId));
+  }
+
+    // Alterar ou desativar plano
+  @Patch('change/:planId') // planId opcional para desativar
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Plano alterado com sucesso' })
+  @ApiResponse({ status: 400, description: 'Dados inválidos' })
+  async changePlan(@Param('planId') planId: string, @Req() req) {
+    const userId = req.user.sub;
+    return this.plansService.changePlan(userId, planId ? parseInt(planId) : null);
   }
 }
