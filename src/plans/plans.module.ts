4e516c6c163b17eeec29655b2f88e46a28ac6246
Rodrigo Nogueira <rodrigoroboticsadm@gmail.com>
2025-10-29 11:21:28 -0300

    chore(plans): import AuthModule with forwardRef
    Use forwardRef to handle potential circular dependencies between PlansModule and AuthModule.

--- src/plans/plans.module.ts
+++ src/plans/plans.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PlansService } from './plans.service';
 import { PlansController } from './plans.controller';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { AuthModule } from '../auth/auth.module'
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [PlansController],
   providers: [PlansService, PrismaService],
 })
